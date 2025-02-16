f95ef959391697ed45e964e01a14e4a1493460c3
J-Spinks <jms2895@outlook.com>
2025-02-16 14:05:59 +0000

    added book object construction and createNewBook function

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log("test")
+function Book(author, title, pages, readStatus) {
+  this.author = author,
+  this.title = title,
+  this.pages = pages,
+  this.readStatus = readStatus
+};
+
+let library = [];
+
+function createNewBook(author, title, pages, readStatus) {
+  const book = new Book(author, title, pages, readStatus);
+  library.push(book);
+};
+
+createNewBook("test", "test", 100, false);
+createNewBook("test", "test", 2000, true);
+
