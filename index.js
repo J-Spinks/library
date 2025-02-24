56038add80499d2b1aaf240a0a070a7de07897d4
J-Spinks <jms2895@outlook.com>
2025-02-24 10:40:43 +0000

    added form with toggle

--- index.js
+++ index.js
@@ -1,17 +1,28 @@
-function Book(author, title, pages, readStatus) {
-  this.author = author,
-  this.title = title,
-  this.pages = pages,
-  this.readStatus = readStatus
-};
+// function Book(author, title, pages, readStatus) {
+//   this.author = author,
+//   this.title = title,
+//   this.pages = pages,
+//   this.readStatus = readStatus
+// };
 
-let library = [];
+// let library = [];
 
-function createNewBook(author, title, pages, readStatus) {
-  const book = new Book(author, title, pages, readStatus);
-  library.push(book);
-};
+// function createNewBook(author, title, pages, readStatus) {
+//   const book = new Book(author, title, pages, readStatus);
+//   library.push(book);
+// };
 
-createNewBook("test", "test", 100, false);
-createNewBook("test", "test", 2000, true);
+const newBookBtn = document.getElementById("new-book-btn");
+const cancelBookBtn = document.getElementById("cancel-book-btn");
+const form = document.querySelector(".form-structure");
 
+// toggle this later?
+newBookBtn.addEventListener("click", (e) => {
+  form.style.display = "block";
+  newBookBtn.style.display = "none";
+});
+
+cancelBookBtn.addEventListener("click", (e) => {
+  form.style.display = "none";
+  newBookBtn.style.display = "block";
+})
