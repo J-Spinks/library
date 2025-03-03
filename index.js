36b9743f46f3921dc4870be8667c03432186f490
J-Spinks <jms2895@outlook.com>
2025-03-03 13:15:34 +0000

    added create new book via form submission

--- index.js
+++ index.js
@@ -5,9 +5,9 @@ function Book(author, title, pages, readStatus) {
   this.readStatus = readStatus
 };
 
-const book1 = new Book("Ian Flemming", "James Bond", 100, false)
-const book2 = new Book("JRR Tolkien", "Fellowship of the ring", 800, true)
-const book3 = new Book("JRR Tolkien", "Fellowship of the ring", 800, true)
+const book1 = new Book("Ian Flemming", "James Bond", 100, "No")
+const book2 = new Book("JRR Tolkien", "Fellowship of the ring", 800, "no")
+const book3 = new Book("JRR Tolkien", "Fellowship of the ring", 800, "yes")
 
 
 let library = [book1, book2, book3];
@@ -21,6 +21,8 @@ const newBookBtn = document.getElementById("new-book-btn");
 const cancelBookBtn = document.getElementById("cancel-book-btn");
 const form = document.querySelector(".form-structure");
 const libraryContainer = document.getElementById("lib-container");
+const submitBtn = document.getElementById("submit-btn");
+const formData = document.getElementById("form-data")
 
 
 
@@ -35,41 +37,42 @@ cancelBookBtn.addEventListener("click", (e) => {
   newBookBtn.style.display = "block";
 });
 
-// for (let i = 0; i < library.length; i++) {
-//   const book = document.createElement("div");
-//   book.classList.add("book");
-//   const author = document.createElement("p");
-//   author.classList.add("author")
-//   book.value = `${library[i].author}`
-//   libraryContainer.appendChild(book)
-// }
+function printLibrary(){
+  library.forEach((book, index) =>{
+    const bookDiv = document.createElement("div");
+    const updateBtn = document.createElement("button")
+    updateBtn.classList.add("updateBtn");
+    updateBtn.textContent = "Update read status"
+    bookDiv.classList.add("book");
+    bookDiv.classList.add(`id${index + 1}`);
+    const author = document.createElement("p");
+    author.classList.add("author");
+    const title = document.createElement("p");
+    title.classList.add("title");
+    const pages = document.createElement("p");
+    pages.classList.add("pages");
+    const read = document.createElement("p");
+    read.classList.add("read");
+    author.innerText = `Author: ${book.author}`;
+    title.innerText = `Title: ${book.title}`;
+    pages.innerText = `Pages: ${book.pages}`;
+    read.innerText = `Read: ${book.readStatus}`;
+    bookDiv.appendChild(author);
+    bookDiv.appendChild(title);
+    bookDiv.appendChild(pages);
+    bookDiv.appendChild(read);
+    bookDiv.appendChild(updateBtn);
+    libraryContainer.appendChild(bookDiv)
+  });
+}
 
-library.forEach((book, index) =>{
-  const bookDiv = document.createElement("div");
-  const updateBtn = document.createElement("button")
-  updateBtn.classList.add("updateBtn");
-  updateBtn.textContent = "Update read status"
-  bookDiv.classList.add("book");
-  bookDiv.classList.add(`id${index + 1}`);
-  const author = document.createElement("p");
-  author.classList.add("author");
-  const title = document.createElement("p");
-  title.classList.add("title");
-  const pages = document.createElement("p");
-  pages.classList.add("pages");
-  const read = document.createElement("p");
-  read.classList.add("read");
-  author.innerText = `Author: ${book.author}`;
-  title.innerText = `Title: ${book.title}`;
-  pages.innerText = `Pages: ${book.pages}`;
-  read.innerText = `Read: ${
-    book.readStatus ? "Yes" : "No"
-  }`;
-  bookDiv.appendChild(author);
-  bookDiv.appendChild(title);
-  bookDiv.appendChild(pages);
-  bookDiv.appendChild(read);
-  bookDiv.appendChild(updateBtn);
-  libraryContainer.appendChild(bookDiv)
+formData.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const author = document.getElementById("author-input").value;
+  const title = document.getElementById("title-input").value;
+  const pages = document.getElementById("pages-input").value;
+  const readStatus = document.getElementById("read-input").value;
+  createNewBook(author, title, pages, readStatus);
+  printLibrary();
 
 });
